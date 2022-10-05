290291a7a527a64427cff6caaebae057d9ee1e69
ATN35 <contact.atn35@gmail.com>
2022-10-05 03:31:34 -0700

    test101

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -15,14 +15,10 @@ int main(void)
 	for (d = 0; d < 100; d++)
 	{
 		putchar((d / 10) + '0');
-		putchar((d % 10) + '0');
 		if (d != 99)
 		{
 			putchar(',');
-			putchar(' ');
 		}
-	}
-	putchar('\n');
-
+		{
 	return (0);
 }
