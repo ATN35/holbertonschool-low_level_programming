0a03af7cf717200b87d3c4f5acf2021e7a88fe7b
ATN35 <contact.atn35@gmail.com>
2022-10-06 07:00:33 -0700

    test105

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the alphabet.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
-	for (i = 0; i < 100 ; i++)
-		}
-		if (i / 10) < (i % 10)
-		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-		}
-		if (i < 89 && (i / 10) < (i % 10)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-			{
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }
