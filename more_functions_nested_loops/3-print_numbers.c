4e69d5ac990c3187bce9b668cfb252e9d5cd9a1b
ATN35 <contact.atn35@gmail.com>
2022-10-07 11:33:40 -0700

    test138

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
+	
