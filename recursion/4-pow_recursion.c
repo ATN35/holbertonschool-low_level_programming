8c6d8b0000cc85f53529c092ca4ec055aa8fc8e1
ATN35 <contact.atn35@gmail.com>
2022-10-28 06:32:41 -0700

    test150

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,12 @@
+w#include "main.h"
+
+/**
+ *_pow_recursion - a function that returns the value of x raised to the power of y.
+ *@x: x
+ *@y: y
+ *Return: something
+ */
+
+int _pow_recursion(int x, int y)
+	if
+	
