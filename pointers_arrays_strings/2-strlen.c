be2ed4e9cf9b252ff8864e0ed363044dc5c01367
ATN35 <contact.atn35@gmail.com>
2022-10-31 15:59:33 -0700

    test155

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen - a function that returns the length of a string
+ *@s: s
+ *Return: Nothing
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	while (*s != '\0')
+		lenght++;
+	return (lenght);
+}
