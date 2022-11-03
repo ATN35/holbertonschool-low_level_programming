a2e4a55cbbf2d2997bd9424c9e1fcca0c4e63f11
ATN35 <contact.atn35@gmail.com>
2022-11-03 05:52:05 -0700

    test157

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_rev -  a function that prints a string, in reverse
+ *@s: s
+ *return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int a;
+	for (a = 0; s[a] != 0; a++)
+		for (a = a - 1; c >= 0; a--)
+			_putchar(s[a]);
+	_putchar('\n');
+}
