a433f0cdace7c502d78931ce6ebcd28ee834379d
ATN35 <contact.atn35@gmail.com>
2022-11-03 13:55:16 -0700

    test163

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -7,14 +7,14 @@
  * @n: n
  * return: Nothing
  */
- 
- void print_array(int *a, int n)
+
+void print_array(int *a, int n)
 {
 	int i;
 
-	while (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", *a[i]);
+		printf("%d", a[i]);
 		if (i != (n - 1))
 			printf(", ");
 	}
