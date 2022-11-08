766b80e7b0085fd6ce6db91954abf0fe5079d16e
ATN35 <contact.atn35@gmail.com>
2022-11-08 08:06:23 -0800

    test180

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -7,13 +7,15 @@
  * @n: n
  * Return: dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 00; i < n; i++)
-		src[i] != '\0';
+	for (i = 0; i < n; i++)
+	{
 		dest[i] = src[i];
+	}
 	for (; i < n; i++)
 		dest[i] = '\0';
 	return (dest);
--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- *string_toupper - a function that changes all lowercase letters of a string
+ *string_toupper - a function
  *@str: string
  *Return: str
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
-	char *str = s;
+	int i = 0;
 
-	while (*s)
+	while (str[i] != '\0')
 	{
-		if (*s >= 'a' && *s <= 'z')
-			*s -= 32;
-		s++;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
 	}
 	return (str);
 }
