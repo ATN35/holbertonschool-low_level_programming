09459af095c60231da3e183b2efb6d8668c69f6b
ATN35 <contact.atn35@gmail.com>
2022-11-08 06:14:29 -0800

    test175

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *strspn - a function that gets the length of a prefix substring
+ *@s: s
+ *@accept: accept
+ *Return: i
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int num;
+	int i, a, b;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		b = 0;
+		for (a = 0; accept[a] != '\0'; a++)
+		{
+			if (s[i] == accept[a])
+				b = 1;
+		}
+		a = 0;
+		if (b == 0)
+			break;
+		num++;
+		i++;
+	}
+	return (i);
+}
