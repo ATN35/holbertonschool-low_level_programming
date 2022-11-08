7239f5f42644a70097d9ac58a56b2d3fb6224938
ATN35 <contact.atn35@gmail.com>
2022-11-08 08:34:58 -0800

    test183

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337
+ * @str: str
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	int a = 0, b = 0;
+	char i[5] = {'A', 'E', 'O', 'T', 'L'};
+	char j[5] = {'4', '3', '0', '7', '1'};
+
+	while (str[a])
+	{
+		b = 0;
+		while (b < 5)
+		{
+			if (str[a] == i[b] || str[a] - 32 == i[b])
+			{
+				str[a] = j[b];
+			}
+			b++;
+		}
+		a++;
+	}
+	return (str);
+}
