02b315680dd56c3ffa705f4ddfb2a617c83adf41
ATN35 <contact.atn35@gmail.com>
2022-11-08 12:53:22 -0800

    test186

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_sqrt_recursion - a function that returns the natural square root of a number
+ * @n: n
+ * Return: 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (0(n, 1));
+}
