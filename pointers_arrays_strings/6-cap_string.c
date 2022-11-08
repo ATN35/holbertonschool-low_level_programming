389acebc0fa88bed029f926781a6ad87662f3e44
ATN35 <contact.atn35@gmail.com>
2022-11-08 08:18:19 -0800

    test181

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,8 +2,29 @@
 
 /**
  *leet - a function that encodes a string into 1337
- *@:
- *Return:
+ *@str: str
+ *Return: str
  */
 
-char *leet(char *)
+char *leet(char *str)
+{
+	int a = 0, b;
+	char d1[13] = {' ', '\n', '\t', ',', ';', '.', '!', '?',
+		      '"', '(', ')', '{', '}' };
+	int d2 = 13;
+
+	while (str[a])
+	{
+		b = 0;
+		while (b < d2)
+		{
+			if ((a == 0 || str[a - 1] == d1[b]) && (str[a] >= 97 && str[a] <= 122))
+			{
+				str[a] -= 32;
+			}
+			b++;
+		}
+		a++;
+	}
+	return (str);
+}
