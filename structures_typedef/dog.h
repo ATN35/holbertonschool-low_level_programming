eb56689c1e788ef765684ac8820105a62b4ebe40
ATN35 <contact.atn35@gmail.com>
2022-11-10 06:08:20 -0800

    test197

---
+++ structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog - structure of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+/**
+ * dog_t - typedef of dog_t
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+void free_dog(dog_t *d);
+
+#endif
