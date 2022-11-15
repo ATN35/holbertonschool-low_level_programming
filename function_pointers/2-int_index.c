3593dd035bf3068970f6853103975abb42bce7de
ATN35 <contact.atn35@gmail.com>
2022-11-15 03:47:11 -0800

    test206

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ *int_index - a function that searches for an integer
+ *@array: array
+ *@size: size
+ *@cmp: cmp
+ *Return: -1 or i
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+	return (-1);
+}
