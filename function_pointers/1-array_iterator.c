14f212713997ea1507147f220c996bedfe72e96c
ATN35 <contact.atn35@gmail.com>
2022-11-15 03:03:47 -0800

    test205

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ *array_iterator - a function that executes a function
+ *@array: array
+ *@size: size
+ *@action: action
+ *Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
