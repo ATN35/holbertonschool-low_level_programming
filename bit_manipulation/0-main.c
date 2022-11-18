cd3bcc924610bc6e23b806d20abfd7eb01310e79
ATN35 <contact.atn35@gmail.com>
2022-11-18 14:35:36 -0800

    test219

---
+++ bit_manipulation/0-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * binary_to_uint - a function that converts a binary number to an unsigned int
+ * @b: binary
+ * Return: 0 or a
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int a = 0, x = 1;
+	int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*(b + i))
+	{
+		if (*(b + i) != '0' && *(b + i) != '1')
+			return (0);
+		i++;
+	}
+	for (i--; i >= 0; i--)
+	{
+		if (*(b + i) == '1')
+			a = a + x;
+		x = x * 2;
+	}
+	return (a);
+}
