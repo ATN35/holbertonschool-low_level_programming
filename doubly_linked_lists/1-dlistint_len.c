9f61a652322ff1e537ec6a10c489a8f3059a615d
ATN35 <contact.atn35@gmail.com>
2022-11-26 10:00:13 -0800

    test231

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - a function that that returns the number of elements
+ * @h: pointer to node
+ * Return: nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
