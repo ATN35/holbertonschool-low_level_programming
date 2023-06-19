a9f80412e90c46139d898126125180319b78073b
ATN35 <5613@holbertonstudents.com>
2023-06-19 03:13:34 -0700

    This program will assign a random number to the variable n each time it is executed

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -19,11 +19,11 @@ printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%dis zero\n", n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%d  is negative\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
