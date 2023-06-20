2cbdd505d171bd3e28d0cd978bda5b6b3063de6e
ATN35 <5613@holbertonstudents.com>
2023-06-20 04:15:42 -0700

    Write a program that prints _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char hol[] = "_putchar";
+	int n;
+
+	n = 0;
+
+	while (n <= 7)
+	{
+		_putchar(hol[n]);
+		n++;
+	}
+	_putchar('\n');
+	return (0);
+}
