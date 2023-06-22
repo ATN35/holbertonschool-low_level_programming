c7375cf280eada9409b4dd52be69291e0939a698
ATN35 <5613@holbertonstudents.com>
2023-06-22 14:09:32 -0700

    Write a function that prints all natural numbers from n to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,28 +8,17 @@
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	while (n != 98)
 	{
-		for (; n > 98; n--)
-			}
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	else if (n < 98)
-	{
-		for (; n < 98; n++)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
+
+		n = (n > 98) ? n - 1 : n + 1;
 	}
 
-	_putchar((n / 10) + '0');
-	_putchar((n % 10) + '0');
+	_putchar('9');
+	_putchar('8');
 	_putchar('\n');
 }
