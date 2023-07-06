53c50085833a04c520803f03f07bf4d46374fde1
ATN35 <5613@holbertonstudents.com>
2023-07-06 05:09:49 -0700

    Write a program that prints all arguments it receives

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - a program that prints its name
  * @argc: arguement count
  * @argv: argument value, a strings that come after calling function
- * Return: Always 0 (Success)
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 	{
--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Write a program that prints the number of arguments passed into it
+ * main - a program that prints the number of arguments passed into it
  * @argc: arguement count
  * @argv: argument value, a strings that come after calling function
- * Return: Always 0 (Success)
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
