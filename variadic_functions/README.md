a78719e6176ab8c476da251e141bacd9246388de
ATN35 <5613@holbertonstudents.com>
2023-07-21 11:36:25 -0700

    Write a function that returns the sum of all its parameters

---
+++ variadic_functions/README.md
@@ -0,0 +1,9 @@
+Tasks:
+
+0. Beauty is variable, ugliness is constant: Write a function that returns the sum of all its parameters.
+
+1. To be is to be the value of a variable: Write a function that prints numbers.
+
+2. One woman's constant is another woman's variable: Write a function that prints strings.
+
+3. To be is a to be the value of a variable: Write a function that prints anything.
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,26 @@
+#ifndef __variadic__functions__h__
+#define __variadic__functions__h__
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+void print_c(va_list arg);
+void print_i(va_list arg);
+void print_f(va_list arg);
+void print_s(va_list arg);
+
+/**
+ * struct format - struct format
+ * @type: the type
+ * @f: the function associated
+ */
+typedef struct format
+{
+	char *type;
+	void (*f)(va_list);
+} form;
+
+#endif
